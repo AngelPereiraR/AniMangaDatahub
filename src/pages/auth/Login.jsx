798f25a66f54a6491e90fa0348701278df0144cf
Ángel Pereira <ampr2003@gmail.com>
2024-11-25 13:28:45 +0100

    "Refactored authentication components to use Formik for form management and validation."

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,17 +1,19 @@
-import React, { useContext, useEffect, useState } from "react";
-import { FormModeContext } from "../../context/FormModeContext";
-import { Bounce, toast } from "react-toastify";
+import React, { useContext, useEffect } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Input from "../../components/auth/Input";
 import Button from "../../components/shared/Button";
-import { NavLink, useNavigate } from "react-router-dom";
-import "./Login.css";
-import { login } from "../../config/Firebase";
+import { Bounce, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FormModeContext } from "../../context/FormModeContext";
 import { UserContext } from "../../context/UserContext";
+import { useNavigate, NavLink } from "react-router-dom";
+import { login } from "../../config/Firebase";
+import "./Login.css";
 
 const Login = () => {
   const { updateFormMode } = useContext(FormModeContext);
   const { user } = useContext(UserContext);
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,75 +21,29 @@ const Login = () => {
     if (user !== null) {
       navigate("/");
     }
-  }, [user]);
+  }, [user, updateFormMode, navigate]);
 
-  // Estado para almacenar los datos del formulario
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "El correo electrónico no tiene un formato válido"
+      )
+      .required("El correo es obligatorio"),
+    password: Yup.string()
+      .min(6, "La contraseña tiene que tener como mínimo 6 caracteres")
+      .required("La contraseña no puede estar vacía"),
   });
 
-  // Estado para manejar los errores de validación
-  const [errors, setErrors] = useState({
+  const initialValues = {
     email: "",
     password: "",
-  });
-
-  // Maneja el cambio en los campos del formulario
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
   };
 
-  // Función de validación para cada campo individual
-  const validateField = (name, value) => {
-    const newErrors = { ...errors };
-    if (name === "email") {
-      newErrors.email = !value.trim()
-        ? "El correo es obligatorio"
-        : !/\S+@\S+\.\S+/.test(value)
-        ? "El correo electrónico no tiene un formato válido"
-        : "";
-    }
-    if (name === "password") {
-      newErrors.password = !value.trim()
-        ? "La contraseña no puede estar vacía"
-        : "";
-    }
-    setErrors(newErrors);
-  };
-
-  // Maneja el evento de pérdida de foco (blur) en los campos para validar
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
-    validateField(name, value);
-  };
-
-  // Función que valida todos los campos del formulario antes de enviar
-  const validateForm = () => {
-    const newErrors = {};
-
-    if (!formData.email.trim()) newErrors.email = "El correo es obligatorio";
-    else if (!/\S+@\S+\.\S+/.test(formData.email))
-      newErrors.email = "El correo electrónico no tiene un formato válido";
-    if (!formData.password.trim())
-      newErrors.password = "La contraseña no puede estar vacía";
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
-
-  // Función que se ejecuta al enviar el formulario
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (validateForm()) {
-      await login({ email: formData.email, password: formData.password });
-
-      // Muestra una notificación de éxito usando react-toastify
-      toast.success("Login completado con éxito", {
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      await login(values);
+      toast.success("Inicio de sesión completado con éxito", {
         position: "bottom-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -96,15 +52,22 @@ const Login = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        transition: Bounce, // Transición para la notificación
+        transition: Bounce,
       });
-
-      // Limpia el formulario y regresa a la página de inicio
-      setTimeout(() => {
-        setFormData({ email: "", password: "" });
-        updateFormMode(false);
-        navigate("/");
-      }, 200);
+      resetForm();
+      navigate("/");
+    } catch (error) {
+      toast.error("Error al iniciar sesión. Verifica tus credenciales.", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
   };
 
@@ -112,44 +75,51 @@ const Login = () => {
     <main className="login-main">
       <section className="login">
         <h2 className="login__heading">Iniciar sesión</h2>
-        <form className="login__form" onSubmit={handleSubmit}>
-          <Input
-            label="Correo electrónico"
-            type="email"
-            placeholder="correo@dominio.com"
-            value={formData.email}
-            errorMessage={errors.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="email"
-          />
-          <Input
-            label="Contraseña"
-            type="password"
-            placeholder="contraseña"
-            value={formData.password}
-            errorMessage={errors.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="password"
-          />
-          {/* Botón de envío */}
-          <Button label="Iniciar sesión" type="submit" variant="primary" />
-          <nav className="form__links">
-            <ul className="links__list">
-              <li>
-                <NavLink className="list_link" to="/register">
-                  ¿No estás registrado? Regístrate
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="list_link" to="/">
-                  Volver a la pantalla principal
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleChange, handleBlur, values }) => (
+            <Form className="login__form">
+              <Input
+                label="Correo electrónico"
+                type="email"
+                placeholder="correo@dominio.com"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={<ErrorMessage name="email" component="p" />}
+              />
+              <Input
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={<ErrorMessage name="password" component="p" />}
+              />
+              <Button label="Iniciar sesión" type="submit" variant="primary" />
+              <nav className="form__links">
+                <ul className="links__list">
+                  <li>
+                    <NavLink className="list_link" to="/register">
+                      ¿No estás registrado? Regístrate
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="list_link" to="/">
+                      Volver a la pantalla principal
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </Form>
+          )}
+        </Formik>
       </section>
     </main>
   );
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,18 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
-import { FormModeContext } from "../../context/FormModeContext";
-import { NavLink, useNavigate } from "react-router-dom";
-import { Bounce, toast } from "react-toastify";
+import React, { useContext, useEffect } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Input from "../../components/auth/Input";
 import Button from "../../components/shared/Button";
-import "./Register.css";
-import { register } from "../../config/Firebase";
-import TermsAndConditions from "../../components/auth/TermsAndConditions";
+import { Bounce, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FormModeContext } from "../../context/FormModeContext";
 import { UserContext } from "../../context/UserContext";
+import { register, login } from "../../config/Firebase";
+import TermsAndConditions from "../../components/auth/TermsAndConditions";
+import { useNavigate, NavLink } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
   const { updateFormMode } = useContext(FormModeContext);
   const { user } = useContext(UserContext);
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,115 +22,41 @@ const Register = () => {
     if (user !== null) {
       navigate("/");
     }
-  }, [user]);
+  }, [user, updateFormMode, navigate]);
 
-  // Estado para almacenar los datos del formulario
-  const [formData, setFormData] = useState({
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("El nombre de usuario es obligatorio"),
+    email: Yup.string()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "El correo electrónico no tiene un formato válido"
+      )
+      .required("El correo es obligatorio"),
+    password: Yup.string()
+      .min(6, "La contraseña tiene que tener como mínimo 6 caracteres")
+      .required("La contraseña no puede estar vacía"),
+    confirmPassword: Yup.string()
+      .min(6, "La contraseña tiene que tener como mínimo 6 caracteres")
+      .oneOf([Yup.ref("password")], "Las contraseñas deben coincidir")
+      .required("La confirmación de la contraseña es obligatoria"),
+    termsAndConditions: Yup.bool()
+      .oneOf([true], "Debes aceptar los términos y condiciones")
+      .required("Debes aceptar los términos y condiciones"),
+  });
+
+  const initialValues = {
     username: "",
     email: "",
     password: "",
     confirmPassword: "",
     termsAndConditions: false,
-  });
-
-  // Estado para manejar los errores de validación
-  const [errors, setErrors] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    termsAndConditions: "",
-  });
-
-  // Maneja el cambio en los campos del formulario
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setFormData({
-      ...formData,
-      [name]: name === "termsAndConditions" ? value === "✔" : value,
-    });
   };
 
-  // Función de validación para cada campo individual
-  const validateField = (name, value) => {
-    const newErrors = { ...errors };
-    if (name === "username") {
-      newErrors.username = !value.trim()
-        ? "El nombre de usuario es obligatorio"
-        : "";
-    }
-    if (name === "email") {
-      newErrors.email = !value.trim()
-        ? "El correo es obligatorio"
-        : !/\S+@\S+\.\S+/.test(value)
-        ? "El correo electrónico no tiene un formato válido"
-        : "";
-    }
-    if (name === "password") {
-      newErrors.password = !value.trim()
-        ? "La contraseña no puede estar vacía"
-        : "";
-    }
-    if (name === "confirmPassword") {
-      newErrors.confirmPassword = !value.trim()
-        ? "La confirmación de la contraseña no puede estar vacía"
-        : formData.password !== formData.confirmPassword
-        ? "La contraseña y la confirmación de la contraseña deben ser iguales"
-        : "";
-    }
-    if (name === "termAndConditions") {
-      newErrors.termsAndConditions = !value.trim()
-        ? "Debes aceptar los términos y condiciones"
-        : "";
-    }
-    setErrors(newErrors);
-  };
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      await register({ email: values.email, password: values.password });
+      await login({ email: values.email, password: values.password });
 
-  // Maneja el evento de pérdida de foco (blur) en los campos para validar
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
-
-    if (name === "termsAndConditions") {
-      validateField(name, value === "✔");
-    } else {
-      validateField(name, value);
-    }
-  };
-
-  // Función que valida todos los campos del formulario antes de enviar
-  const validateForm = () => {
-    const newErrors = {};
-
-    if (!formData.username.trim())
-      newErrors.username = "El nombre de usuario es obligatorio";
-    if (!formData.email.trim()) newErrors.email = "El correo es obligatorio";
-    else if (!/\S+@\S+\.\S+/.test(formData.email))
-      newErrors.email = "El correo electrónico no tiene un formato válido";
-    if (!formData.password.trim())
-      newErrors.password = "La contraseña no puede estar vacía";
-    if (!formData.confirmPassword.trim())
-      newErrors.confirmPassword =
-        "La confirmación de la contraseña no puede estar vacía";
-    else if (formData.password !== formData.confirmPassword)
-      newErrors.confirmPassword =
-        "La contraseña y la confirmación de la contraseña deben ser iguales";
-    if (!formData.termsAndConditions)
-      newErrors.termsAndConditions = "Debes aceptar los términos y condiciones";
-
-    console.log(newErrors);
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
-
-  // Función que se ejecuta al enviar el formulario
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (validateForm()) {
-      await register({ email: formData.email, password: formData.password });
-      await login({ email: formData.email, password: formData.password });
-
-      // Muestra una notificación de éxito usando react-toastify
       toast.success("Registro completado con éxito", {
         position: "bottom-right",
         autoClose: 5000,
@@ -138,21 +66,22 @@ const Register = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        transition: Bounce, // Transición para la notificación
+        transition: Bounce,
       });
-
-      // Limpia el formulario y regresa a la página de inicio
-      setTimeout(() => {
-        setFormData({
-          username: "",
-          email: "",
-          password: "",
-          confirmPassword: "",
-          termAndConditions: false,
-        });
-        updateFormMode(false);
-        navigate("/");
-      }, 200);
+      resetForm();
+      navigate("/");
+    } catch (error) {
+      toast.error("Error al registrarse. Intenta de nuevo.", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
   };
 
@@ -160,70 +89,82 @@ const Register = () => {
     <main>
       <section className="register">
         <h2 className="register__heading">Registrar</h2>
-        <form className="register__form" onSubmit={handleSubmit}>
-          <Input
-            label="Nombre de usuario"
-            type="text"
-            placeholder="Usuario"
-            value={formData.username}
-            errorMessage={errors.username}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="username"
-          />
-          <Input
-            label="Correo electrónico"
-            type="email"
-            placeholder="correo@dominio.com"
-            value={formData.email}
-            errorMessage={errors.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="email"
-          />
-          <Input
-            label="Contraseña"
-            type="password"
-            placeholder="contraseña"
-            value={formData.password}
-            errorMessage={errors.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="password"
-          />
-          <Input
-            label="Repetir contraseña"
-            type="password"
-            placeholder="contraseña"
-            value={formData.confirmPassword}
-            errorMessage={errors.confirmPassword}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name="confirmPassword"
-          />
-          <TermsAndConditions
-            onChange={handleChange}
-            onBlur={handleBlur}
-            errorMessage={errors.termsAndConditions}
-            name="termsAndConditions"
-          />
-          {/* Botón de envío */}
-          <Button label="Registrarse" type="submit" variant="primary" />
-          <nav className="form__links">
-            <ul className="links__list">
-              <li>
-                <NavLink className="list_link" to="/login">
-                  ¿Ya estás registrado? Inicia sesión
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="list_link" to="/">
-                  Volver a la pantalla principal
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleChange, handleBlur, values }) => (
+            <Form className="register__form">
+              <Input
+                label="Nombre de usuario"
+                type="text"
+                placeholder="Usuario"
+                name="username"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={<ErrorMessage name="username" component="p" />}
+              />
+              <Input
+                label="Correo electrónico"
+                type="email"
+                placeholder="correo@dominio.com"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={<ErrorMessage name="email" component="p" />}
+              />
+              <Input
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={<ErrorMessage name="password" component="p" />}
+              />
+              <Input
+                label="Repetir contraseña"
+                type="password"
+                placeholder="contraseña"
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={
+                  <ErrorMessage name="confirmPassword" component="p" />
+                }
+              />
+              <TermsAndConditions
+                name="termsAndConditions"
+                value={values.termsAndConditions}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={
+                  <ErrorMessage name="termsAndConditions" component="p" />
+                }
+              />
+              <Button label="Registrarse" type="submit" variant="primary" />
+              <nav className="form__links">
+                <ul className="links__list">
+                  <li>
+                    <NavLink className="list_link" to="/login">
+                      ¿Ya estás registrado? Inicia sesión
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="list_link" to="/">
+                      Volver a la pantalla principal
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </Form>
+          )}
+        </Formik>
       </section>
     </main>
   );
